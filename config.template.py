32e3b35e0387f9d8e3c902571fb64f39e1e1373f
Inervo <inervo@gmail.com>
2023-02-10 22:03:06 +0000

    Partial cleanup of bengami code

--- config.template.py
+++ config.template.py
@@ -1,5 +1,4 @@
-# get access token from: https://next.bgm.tv/demo/access-token
-BANGUMI_ACCESS_TOKEN = 'gruUsn***************************SUSSn'
+# 
 KOMGA_BASE_URL = "http://IP:PORT"
 KOMGA_EMAIL = "email"
 KOMGA_EMAIL_PASSWORD = "password"
