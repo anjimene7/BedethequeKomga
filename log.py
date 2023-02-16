1dc5997e3ba74daf710d31fa34319a9844fce93b
Inervo <inervo@gmail.com>
2023-02-16 09:28:26 +0000

    log output both to file and stdout

--- log.py
+++ log.py
@@ -1,10 +1,14 @@
 import logging
+import sys
 
 # init logging
 logger = logging.getLogger()
 fh = logging.FileHandler('refreshMetadata.log', encoding='utf-8', mode='w')
+sh = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(filename)-8s : %(lineno)s - %(message)s")
+    "%(asctime)s - %(name)s - %(levelname)s - refreshMetadata.py - %(message)s")
 fh.setFormatter(formatter)
+sh.setFormatter(formatter)
 logger.addHandler(fh)
+logger.addHandler(sh)
 logger.setLevel(logging.INFO)
